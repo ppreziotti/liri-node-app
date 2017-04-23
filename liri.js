84da8ec7559822862a586789e36accfd20d69dd1
ppreziotti <patpreziotti@gmail.com>
2017-04-23 15:51:46 -0400

    completed getSong function

--- liri.js
+++ liri.js
@@ -21,8 +21,8 @@ function getTweets() {
   	if (error) {
       console.log(error);
   	}
-  	// Pulling the tweet's text and time they were created at...need to update 
-  	// to last 20 once I have enough tweets
+  	// Pulling the tweet's text and time they were created at from the appropriate
+  	// screen name...need to update to last 20 once I have enough tweets
   	for (var i = 0; i < tweets.length; i++) {
 	  console.log(tweets[i].text);
 	  console.log(tweets[i].created_at);
@@ -32,21 +32,39 @@ function getTweets() {
 
 function getSong() {
   var args = process.argv;
-  var inputArray = [];
-  for (var i = 3; i < args.length; i++) {
-  	inputArray.push(args[i]);
+  // If args.length equals 3 then no song was entered, use the spotify API lookup method
+  // in order to default to "The Sign" by Ace of Base
+  if (args.length === 3) {
+  	var songId = "0hrBpAOgrt8RXigk83LLNE";
+  	spotify.lookup({type: 'track', id: songId}, function(err, data) {
+  	  if (err) {
+  	  	console.log(err);
+  	  }
+  	  // Display the artist name, song name, preview link, and album name
+	  console.log("Artist: " + data.artists[0].name);
+	  console.log("Song: " + data.name);
+	  console.log("Preview Link: " + data.preview_url);
+	  console.log("Album: " + data.album.name);
+  	});
   }
-  var song = inputArray.join(" ");
-  console.log(song);
-  spotify.search({type: 'track', query: song}, function(err, data) {
-  	if (err) {
-  	  console.log(err);
-  	}
-	console.log(data.tracks.items[0].artists[0].name);
-	console.log(data.name);
-	console.log(data.preview_url);
-	console.log(data.album);
-  });
+  // If a song was entered use the spotify API search method and use the first result
+  else {
+  	var inputArray = [];
+	for (var i = 3; i < args.length; i++) {
+	  inputArray.push(args[i]);
+	}
+  	var song = inputArray.join(" ");
+	spotify.search({type: 'track', query: song}, function(err, data) {
+ 	if (err) {
+ 	  console.log(err);
+	  }
+	  // Display the artist name, song name, preview link, and album name
+	  console.log("Artist: " + data.tracks.items[0].artists[0].name);
+	  console.log("Song: " + data.tracks.items[0].name);
+	  console.log("Preview Link: " + data.tracks.items[0].preview_url);
+	  console.log("Album: " + data.tracks.items[0].album.name);
+	});
+  }
 }
 
 // MAIN PROCESS
